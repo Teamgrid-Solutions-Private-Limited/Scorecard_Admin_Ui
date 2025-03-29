0a5b2f2bb2401b8a02510a5ed5cecb3659edb323
Karan Singh <urskaransingh4@gmail.com>
2025-03-29 17:41:47 +0530

    sorting by type in bills

--- dashboard/components/CustomizedDataGrid.jsx
+++ dashboard/components/CustomizedDataGrid.jsx
@@ -2,17 +2,13 @@ import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import { Avatar, Box } from "@mui/material";
+import { Avatar,Box } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { useNavigate } from "react-router-dom";
 
-export default function CustomizedDataGrid({
-  type,
-  rows,
-  loading,
-  onEdit,
-  onDelete,
-}) {
+
+
+export default function CustomizedDataGrid({ type, rows, loading, onEdit, onDelete }) {
   const getBorderColor = (party) => {
     if (!party) return "gray";
     const lowerParty = party.toLowerCase();
@@ -22,42 +18,28 @@ export default function CustomizedDataGrid({
   };
 
   const navigate = useNavigate();
- 
 
-  const columns =
-    type === "bills"
-      ? [
-          { field: "date", flex: 1, headerName: "Date", minWidth: 150 },
-          { field: "bill", flex: 2, headerName: "Bill", minWidth: 150 },
-          {
-            field: "action",
-            flex: 1,
-            headerName: "Action",
-            minWidth: 150,
-            headerAlign: "right",
-            renderCell: (params) => (
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "flex-end",
-                  flexDirection: "row",
-                  alignItems: "center",
-                  height: "100%",
-                  columnGap: "10px",
-                }}
-              >
-                <EditIcon
-                  onClick={() => onEdit(params.row)}
-                  sx={{ cursor: "pointer", "&:hover": { color: "blue" } }}
-                />
-                <DeleteForeverIcon
-                  onClick={() => onDelete(params.row)}
-                  sx={{ cursor: "pointer", "&:hover": { color: "red" } }}
-                />
-              </div>
-            ),
-          },
-        ]
+
+
+  const columns = type === "bills"
+    ? [
+        { field: "date", flex: 1, headerName: "Date", minWidth: 150 },
+        { field: "bill", flex: 2, headerName: "Bill", minWidth: 150 },
+        { field: "billsType", flex: 1, headerName: "Type", minWidth: 150 ,headerAlign:"center", align: "center"},
+        {
+          field: "action",
+          flex: 1,
+          headerName: "Action",
+          minWidth: 150,
+          headerAlign: "right",
+          renderCell: (params) => (
+            <div style={{ display: "flex", justifyContent: "flex-end", columnGap: "10px" }}>
+              <EditIcon onClick={() => onEdit(params.row)} sx={{ cursor: "pointer", "&:hover": { color: "blue" } }} />
+              <DeleteForeverIcon onClick={() => onDelete(params.row)} sx={{ cursor: "pointer", "&:hover": { color: "red" } }} />
+            </div>
+          ),
+        },
+      ]
       : [
           {
             field: "name",
@@ -174,22 +156,22 @@ export default function CustomizedDataGrid({
           },
         ];
 
-  return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        loading={loading}
-        getRowId={(row) => row._id}
-        initialState={{ pagination: { paginationModel: { pageSize: 20 } } }}
-        pageSizeOptions={[10, 20, 50]}
-        disableColumnFilter
-        disableColumnSelector
-        disableDensitySelector
-        disableColumnResize
-        disableRowSelectionOnClick
-        sx={{ "& .MuiDataGrid-row": { height: 90, alignItems: "center" } }}
-      />
-    </div>
-  );
-}
+        return (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              loading={loading}
+              getRowId={(row) => row._id}
+              initialState={{ pagination: { paginationModel: { pageSize: 20 } } }}
+              pageSizeOptions={[10, 20, 50]}
+              disableColumnFilter
+              disableColumnSelector
+              disableDensitySelector
+              disableColumnResize
+              disableRowSelectionOnClick
+              sx={{ "& .MuiDataGrid-row": { height: 90, alignItems: "center" } }}
+            />
+          </div>
+        );
+      }
