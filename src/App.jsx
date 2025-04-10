1a516e48b0baf47756726df2d08e5ddb968fe1ee
sufian257 <md.sufian@teamgrid.io>
2025-04-10 19:33:09 +0530

    applied private routing (#48)
    Co-authored-by: sufian2000168 <mdsufian1305@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -4,95 +4,41 @@ import {
   Routes,
   Route,
   Navigate,
-} from "react-router-dom";
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from './ProTip';
-import Copyright from './Copyright';
-import SaveSenetors from "/dashboard/AddSenator";
-import Senator from "/dashboard/Senator";
-import Representative from "/dashboard/Representative";
-import SaveRepresentative from "/dashboard/AddRepresentative";
-import Bill from "/dashboard/Bills";
-import SignIn from "/Authentication/components/SignIn";
+} from 'react-router-dom';
+
+import SaveSenetors from '/dashboard/AddSenator';
+import Senator from '/dashboard/Senator';
+import Representative from '/dashboard/Representative';
+import SaveRepresentative from '/dashboard/AddRepresentative';
+import Bill from '/dashboard/Bills';
+import SignIn from '/Authentication/components/SignIn';
 import AddBill from '../dashboard/AddBill';
 import SearchBill from '../dashboard/SearchBill';
+
+const PrivateRoute = ({ element }) => {
+  const token = localStorage.getItem('token');
+  return token ? element : <Navigate to="/login" />;
+};
 
 export default function App() {
   return (
     <Router basename="/scorecard/admin">
-     <Routes>
-       <Route path="/login" element={<SignIn />} />
-       {/* <Route
-         index
-         element={
-           token ? <Navigate to="/senator" /> : <Navigate to="/login" />
-         }
-       /> */}
+      <Routes>
+        <Route path="/login" element={<SignIn />} />
 
-       <Route
-         path="/"
-         element={
-           <Senator/>
-         }
-       />
-       <Route
-         path="add-senator"
-         element={
-           <SaveSenetors />
-         }
-       />
-       <Route
-         path="edit-senator/:id"
-         element={
-           <SaveSenetors />
-         }
-       />
-       <Route
-         path="representative"
-         element={
-           <Representative/>
-         }
-       />
-       <Route
-         path="add-representative"
-         element={
-           <SaveRepresentative/>
-         }
-       />
-        <Route
-         path="edit-representative/:id"
-         element={
-           <SaveRepresentative/>
-         }
-       />
-       <Route
-         path="bills"
-         element={
-           <Bill/>
-         }
-       />
-        <Route
-         path="/add-bill"
-         element={
-           <AddBill/>
-         }
-       />
-       <Route
-         path="bills/edit-bill/:id"
-         element={
-           <AddBill/>
-         }
-       />
-        <Route path="/search-bills" element={ 
-          
-               
-              <SearchBill /> 
-           
-      } 
-      />
-     </Routes>
-   </Router>
+        <Route path="/" element={<PrivateRoute element={<Senator />} />} />
+        <Route path="add-senator" element={<PrivateRoute element={<SaveSenetors />} />} />
+        <Route path="edit-senator/:id" element={<PrivateRoute element={<SaveSenetors />} />} />
+        <Route path="representative" element={<PrivateRoute element={<Representative />} />} />
+        <Route path="add-representative" element={<PrivateRoute element={<SaveRepresentative />} />} />
+        <Route path="edit-representative/:id" element={<PrivateRoute element={<SaveRepresentative />} />} />
+        <Route path="bills" element={<PrivateRoute element={<Bill />} />} />
+        <Route path="/add-bill" element={<PrivateRoute element={<AddBill />} />} />
+        <Route path="bills/edit-bill/:id" element={<PrivateRoute element={<AddBill />} />} />
+        <Route path="/search-bills" element={<PrivateRoute element={<SearchBill />} />} />
+        
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
