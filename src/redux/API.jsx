ef9e65065c1e070d9e1591658aac036fce08c88f
Sk Saruk Ali <sksaruk.ali@teamgrid.io>
2025-08-12 18:17:39 +0530

    added api url automate backend url

--- src/redux/API.jsx
+++ src/redux/API.jsx
@@ -1,6 +1,6 @@
-
 // File: src/redux/API.jsx
-export const API_URL = 'http://localhost:4000';
+export const API_URL = import.meta.env.VITE_API_URL;
 
+// Uncomment to debug the API URL and environment
 //console.log("✅ API_URL:", API_URL);
 //console.log("✅ Environment:", import.meta.env.MODE);
