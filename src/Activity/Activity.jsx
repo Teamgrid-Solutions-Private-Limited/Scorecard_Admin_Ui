9eb0ad8d9709030925d081ab456c9535ade64b0c
tgs-saruk <sksaruk.ali@teamgrid.io>
2026-01-09 20:12:05 +0530

    `Removed commented out code in Activity and Votes components`

--- src/Activity/Activity.jsx
+++ src/Activity/Activity.jsx
@@ -346,7 +346,7 @@ export default function Activity(props) {
                 >
                   {isBulkEditMode ? "Cancel Bulk Edit" : "Bulk Edit"}
                 </Button>
-                {userRole === "admin" && (
+                {/* {userRole === "admin" && (
                   <Button
                     onClick={() => {
                       setIsAssignMode(!isAssignMode);
@@ -356,7 +356,7 @@ export default function Activity(props) {
                   >
                     {isAssignMode ? "Cancel Assign" : "Assign Activity"}
                   </Button>
-                )}
+                )} */}
                 {userRole === "admin" && (
                   <Button
                     onClick={() => navigate("/search-activities")}
